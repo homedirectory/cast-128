c4a76ad9be967e8616d033ab63dfd339efd2aab9
homedirectory <47361170+homedirectory@users.noreply.github.com>
2022-11-18 21:04:25 +0200

    i'm on little-endian

--- test/test.c
+++ test/test.c
@@ -18,6 +18,8 @@ int test_enc_block(uint64_t* key, uint64_t plain, uint64_t exp_cipher) {
 }
 
 int main(int argc, char **argv) {
-    uint64_t key[2] = { 0x0123456712345678, 0x234567893456789a };
-    test_enc_block(key, 0x0123456789abcdef, 0x238b4fe5847e44b2);
+    //uint64_t key[2] = { 0x0123456712345678, 0x234567893456789a };
+    //test_enc_block(key, 0x0123456789abcdef, 0x238b4fe5847e44b2);
+    uint64_t key[2] = { 0x7856341267452301, 0x9a78563489674523 };
+    test_enc_block(key, 0xefcdab8967452301, 0xb2447e84e54f8b23);
 }
